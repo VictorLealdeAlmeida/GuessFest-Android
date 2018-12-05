ffbac1fea8ad8ebf864a7804bad5b3bc1aca218e
Vinicius Lira <vml@cin.ufpe.br>
2018-12-05 13:20:27 -0300

    integracao com ads da inloco

--- GuessFest/app/src/main/java/com/example/victorleal/guessfest/MainActivity.kt
+++ GuessFest/app/src/main/java/com/example/victorleal/guessfest/MainActivity.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import android.graphics.Color
 import android.graphics.Typeface
 import android.support.v7.widget.CardView
+import android.util.Log
 import android.widget.TextView
 import android.widget.RelativeLayout
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import com.inlocomedia.android.ads.InLocoMedia
+import com.inlocomedia.android.ads.InLocoMediaOptions
+import com.inlocomedia.android.ads.AdError
+import com.inlocomedia.android.ads.AdRequest
+import com.inlocomedia.android.ads.interstitial.InterstitialAd
+import com.inlocomedia.android.ads.interstitial.InterstitialAdListener
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,6 +57,36 @@ class MainActivity : AppCompatActivity() {
         cardLinearLayout.addView(name)
         cardView.addView(cardLinearLayout)
       //  mainLinearLayout.addView(cardView)
+
+        // In Loco Media SDK Init
+        val options = InLocoMediaOptions.getInstance(this)
+
+        // The AppId you acquired in earlier steps
+        options.adsKey = "c0cc2276042bf6a59e7fa18d5363cb6ed673f934e52d9c4f7d0317149892217d"
+
+        // Verbose mode flag, if this is set as true InLocoMedia SDK will let you know about errors on the Logcat
+        options.isLogEnabled = true
+
+        // Development Devices set here are only going to receive test ads
+            //options.setDevelopmentDevices("C55FBEEAAAD1B7E6F89D365283D9483")
+
+        InLocoMedia.init(this, options)
+
+
+        val interstitialAd = InterstitialAd(this)
+        interstitialAd.setInterstitialAdListener(object : InterstitialAdListener() {
+
+            override fun onAdReady(ad: InterstitialAd) {
+                ad.show()
+            }
+
+            override fun onAdError(ad: InterstitialAd, error: AdError) {
+                Log.w("InLocoMedia", "Your interstitial has failed with error: $error")
+            }
+        })
+
+        val adRequest = AdRequest()
+        interstitialAd.loadAd(adRequest)
 
     }
 }
