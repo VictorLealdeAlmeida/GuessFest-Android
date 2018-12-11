c9106314990bdb0cdb4a865d7d7bba5d4749434e
VictorLealdeAlmeida <vlpaa@cin.ufpe.br>
2018-12-11 12:54:07 -0300

    Sets de novos temas + ajustes no layout

--- GuessFest/app/src/main/java/com/example/victorleal/guessfest/listTheme/CardsTheme.kt
+++ GuessFest/app/src/main/java/com/example/victorleal/guessfest/listTheme/CardsTheme.kt
@@ -14,17 +14,19 @@ class CardsTheme() {
     fun getCardsTheme(): List<ThemeItem> {
         val thems: MutableList<ThemeItem> = mutableListOf()
 
-        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer_on))
+        thems.add(ThemeItem("The Beatles","Música", R.drawable.music))
 
-        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer_on))
+        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer))
 
-        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer_on))
+        thems.add(ThemeItem("Celebridades","Geral", R.drawable.soccer))
 
-        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer_on))
+        thems.add(ThemeItem("Música","Música", R.drawable.music))
 
-        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer_on))
+        thems.add(ThemeItem("Anos anos80","História", R.drawable.anos2000))
 
-        thems.add(ThemeItem("Futebol","Esportes", R.drawable.soccer_on))
+        thems.add(ThemeItem("Anos anos90","História", R.drawable.anos2000))
+
+        thems.add(ThemeItem("Anos 2000","História", R.drawable.anos2000))
 
 
 
